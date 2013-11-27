6b0c1af9b43e56d69b5298c90f292b1a5a4f00a9
flavienlaurent <flavien.laurent@gmail.com>
2013-11-27 17:13:25 +0100

    custom property

--- src/com/fourmob/sandbox/activities/MainActivity.java
+++ src/com/fourmob/sandbox/activities/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends Activity {
 
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, RotateDrawableActivity.class));
+                startActivity(new Intent(MainActivity.this, RotateDrawablePost14Activity.class));
             }
         });
 	}
--- src/com/fourmob/sandbox/activities/RotateDrawablePost14Activity.java
+++ src/com/fourmob/sandbox/activities/RotateDrawablePost14Activity.java
@@ -1,23 +1,27 @@
 package com.fourmob.sandbox.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.graphics.drawable.RotateDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Property;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Interpolator;
 import android.widget.TextView;
 
 import com.fourmob.sandbox.R;
-import com.nineoldandroids.animation.Animator;
-import com.nineoldandroids.animation.AnimatorListenerAdapter;
-import com.nineoldandroids.animation.ObjectAnimator;
-import com.nineoldandroids.animation.ValueAnimator;
 
 /**
  * Created by f.laurent on 27/11/13.
  */
-public class RotateDrawableActivity extends Activity {
+@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class RotateDrawablePost14Activity extends Activity {
 
     static final int MAX_LEVEL = 10000;
     static final int MID_MAX_LEVEL = MAX_LEVEL / 2;
@@ -30,7 +34,20 @@ public class RotateDrawableActivity extends Activity {
 
     private RotateDrawable mRotateDrawable;
 
-    private ExpandCollapser mExpandCollapser;
+    private static final Property<View, Integer> VIEW_LAYOUT_HEIGHT =
+            new Property<View, Integer>(Integer.class, "viewLayoutHeight") {
+
+                @Override
+                public void set(View object, Integer value) {
+                    object.getLayoutParams().height = value.intValue();
+                    object.requestLayout();
+                }
+
+                @Override
+                public Integer get(View object) {
+                    return object.getLayoutParams().height;
+                }
+            };
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +58,6 @@ public class RotateDrawableActivity extends Activity {
         mContentHeight = getResources().getDimensionPixelSize(R.dimen.content_height);
         mTextView = (TextView) findViewById(R.id.textView);
         mContent = findViewById(R.id.content);
-        mExpandCollapser = new ExpandCollapser(mContent);
 
         mRotateDrawable = (RotateDrawable) getResources().getDrawable(R.drawable.rotate_drawable);
         mTextView.setCompoundDrawablesWithIntrinsicBounds(null, null, mRotateDrawable, null);
@@ -50,7 +66,7 @@ public class RotateDrawableActivity extends Activity {
             @Override
             public void onClick(View v) {
                 mTextView.setEnabled(false);
-                ObjectAnimator expandCollapseAnimator = ObjectAnimator.ofInt(mExpandCollapser, "height", mContent.getHeight(), !mExpanded ? mContentHeight : 0);
+                ObjectAnimator expandCollapseAnimator = ObjectAnimator.ofInt(mContent, VIEW_LAYOUT_HEIGHT, mContent.getHeight(), !mExpanded ? mContentHeight : 0);
                 expandCollapseAnimator.setInterpolator(mSmoothInterpolator);
                 expandCollapseAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
@@ -69,19 +85,5 @@ public class RotateDrawableActivity extends Activity {
                 expandCollapseAnimator.start();
             }
         });
-    }
-
-    private class ExpandCollapser {
-
-        private View mView;
-
-        ExpandCollapser(View view) {
-            mView = view;
-        }
-
-        public void setHeight(int height) {
-            mView.getLayoutParams().height = height;
-            mView.requestLayout();
-        }
     }
 }
